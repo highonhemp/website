1df8e944ed1680e99fcea738c6d3290b80156798
declanhart24 <declanhart24@gmail.com>
2017-02-01 18:38:28 +1000

    DH/TS begin menu

--- src/common/menu.js
+++ src/common/menu.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import MenuItem from './MenuItem';
 
-
-class menu extends Component {
+class Menu extends Component {
   render() {
     return (
-      
+      <MenuItem name="Home"/>
+      <MenuItem name="Brands"/>
+      <MenuItem name="Collab"/>
+      <MenuItem name="Shop"/>
+      <MenuItem name="Blog"/>
     )
   }
 }
+
+export default Menu;
