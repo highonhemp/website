6f2af743de735fbf1ede0144483ad5b1aa055fcf
declanhart24 <declanhart24@gmail.com>
2017-02-12 11:51:54 +1000

    DH styling

--- src/common/Footer.js
+++ src/common/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
   render() {
     return (
       <div className='footer'>
-
+        <h3>Thank you for visiting High On Hemp.</h3>
       </div>
 
     );
--- src/common/MainMenu.js
+++ src/common/MainMenu.js
@@ -5,23 +5,25 @@ import NavItem from 'react-bootstrap/lib/NavItem';
 import NavDropdown from 'react-bootstrap/lib/NavDropdown';
 import MenuItem from 'react-bootstrap/lib/MenuItem';
 import Button from 'react-bootstrap/lib/Button';
+import styles from '../style/MainMenu.css';
+
 class MainMenu extends Component {
   render() {
     let brand = <a href='#'>High on Hemp</a>;
     return (
-      <Navbar inverse toggleNavKey={0}>
-        <Nav right eventKey={0}>
-          <NavItem eventKey={1} href="#">Home</NavItem>
-          <NavDropdown eventKey={3} title="Brands" id="basic-nav-dropdown">
+      <Navbar inverse toggleNavKey={0} className="NavBar">
+        <Nav right eventKey={0} className='Nav'>
+          <NavItem eventKey={1} href="#" className='NavItem'>Home</NavItem>
+          <NavDropdown eventKey={3} title="Brands" id="basic-nav-dropdown" className='NavItem'>
              <MenuItem eventKey={3.1}>Action</MenuItem>
              <MenuItem eventKey={3.2}>Another action</MenuItem>
              <MenuItem eventKey={3.3}>Something else here</MenuItem>
              <MenuItem divider />
              <MenuItem eventKey={3.3}>Separated link</MenuItem>
           </NavDropdown>
-          <NavItem eventKey={3} href="#">Collab</NavItem>
-          <NavItem eventKey={4} href="#">Shop</NavItem>
-          <NavItem eventKey={5} href="#">Blog</NavItem>
+          <NavItem eventKey={3} href="#" className='NavItem'>Collab</NavItem>
+          <NavItem eventKey={4} href="#" className='NavItem'>Shop</NavItem>
+          <NavItem eventKey={5} href="#" className='NavItem'>Blog</NavItem>
         </Nav>
       </Navbar>
     );
